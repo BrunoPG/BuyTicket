b2d2372d2654be0469c733856ebb0c466ba2cbcb
LeoPereiravra <leopereiravra@gmail.com>
2019-04-11 18:51:07 -0300

    Criado projeto com telas e menu
    Criado projeto com telas e menu

---
+++ 2019-03-21 (N1) demo Hello World/buyTicket/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'list', loadChildren: './list/list.module#ListPageModule'  },
+  { path: 'evento', loadChildren: './evento/evento.module#EventoPageModule' },
+  { path: 'sala', loadChildren: './sala/sala.module#SalaPageModule' },
+  { path: 'setor', loadChildren: './setor/setor.module#SetorPageModule' },
+  { path: 'configuracoes', loadChildren: './configuracoes/configuracoes.module#ConfiguracoesPageModule' }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
